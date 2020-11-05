30eb6defc624cd9e98dceba1e6fe3846d2883911
frey44 <73900949+frey44@users.noreply.github.com>
2020-11-05 10:29:45 -0600

    this is test-1

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static <String> void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/projectonesample/sampleone.java
@@ -0,0 +1,10 @@
+package projectonesample;
+
+public class sampleone {
+    private static  System;
+
+    public static <String> void main(String[] args) {
+        System.out.println();
+        
+    }
+}
